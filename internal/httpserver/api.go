90eb9919027905df2f927c33755e26fe96bdfd97
Daniel Kanczuk <danielsussa@gmail.com>
2021-02-02 20:36:40 -0300

    just for test propose

--- internal/httpserver/api.go
+++ internal/httpserver/api.go
@@ -1,6 +1,7 @@
 package httpserver
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net"
@@ -16,17 +17,29 @@ func Start()chan error{
 	chErr := make(chan error)
 	go func() {
 		var err error
-		listener, err = net.Listen("tcp", "localhost:3333")
+		cert, err := tls.LoadX509KeyPair( "/etc/letsencrypt/live/floxy.io/fullchain.pem","/etc/letsencrypt/live/floxy.io/privkey.pem" )
+		if err != nil {
+			return
+		}
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+			Certificates: []tls.Certificate{cert},
+		}
+
+		listener, err = tls.Listen("tcp", "localhost:8443", tlsConfig)
 		if err != nil {
 			chErr <- err
 		}
-		log.Println("start listening at port 3333")
+		log.Println("start listening at port 8443")
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
 				fmt.Println("conn err: ", conn)
 				continue
 			}
+			tlsConn := conn.(*tls.Conn)
+			tlsConn.VerifyHostname()
+
 			fmt.Println("remote conn", conn.RemoteAddr().String())
 			fmt.Println("local conn", conn.LocalAddr().String())
 
